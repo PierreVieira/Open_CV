5a2dae81fc449ff5e01d0afa58901ab261d2aa9a
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2020-05-06 14:41:31 -0300

    [+] Exibindo imagem

---
+++ exemplos/01 - Exibir imagem.py
@@ -0,0 +1,17 @@
+import cv2
+from matplotlib import pyplot as plt
+
+
+def show_image(obj_img):
+    obj_img = cv2.cvtColor(obj_img, cv2.COLOR_BGR2RGB)  # Convertendo BGR para RGB
+    plt.imshow(obj_img)
+    plt.show()
+
+
+def main():
+    obj_img = cv2.imread('../imgs/pierre_vieira.png')
+    show_image(obj_img)
+
+
+if __name__ == '__main__':
+    main()
