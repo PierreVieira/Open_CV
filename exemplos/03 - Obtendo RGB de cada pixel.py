a88da72524c59115c5d306ac388da5f153971771
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2020-05-06 15:08:12 -0300

    [+] Obtendo RGB de cada pixel

---
+++ exemplos/03 - Obtendo RGB de cada pixel.py
@@ -0,0 +1,19 @@
+import cv2
+
+
+def info_rgb_pixel(obj_img, altura, largura):
+    for y in range(altura):  # Fixa a coluna
+        for x in range(largura):  # Percorre a linha
+            # azul, verde, vermelho = obj_img[y][x]  # Dá para fazer desse jeito, porém não é o mais recomendado
+            azul, verde, vermelho = obj_img.item(y, x, 0), obj_img.item(y, x, 1), obj_img.item(y, x, 2)
+            print({'coordenada': (x, y), 'R': vermelho, 'G': verde, 'B': azul})
+
+
+def main():
+    obj_img = cv2.imread('../imgs/pierre_vieira_12_anos.jpg')
+    altura, largura, canais_de_cor = obj_img.shape
+    info_rgb_pixel(obj_img, altura, largura)
+
+
+if __name__ == '__main__':
+    main()
